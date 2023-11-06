d0c17b4fb2a58ab356eefb4d5fec2d3b85b8588e
acpproject021 <147815108+acpproject021@users.noreply.github.com>
2023-11-06 07:12:32 +0000

    simple controller mapping learned

---
+++ demo/src/main/java/com/springdemo/demo/IndexApplication.java
@@ -0,0 +1,14 @@
+package com.springdemo.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexApplication {
+    @RequestMapping(value = "/index")
+    public String index(){
+        return "index";
+    }
+    
+}
